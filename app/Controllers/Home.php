7e2285628d17122adf8e425f2dc7efa0410de40a
Miguel Padrron <mikyeduards@gmail.com>
2021-10-29 09:11:11 -0400

    template organizado y listo header content footer

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,11 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $vistas= view('template/header.php')
+                .view('welcome_message')
+                .view('template/footer.php');//pie
+                
+        return $vistas;
     }
 
     public function nombre()
