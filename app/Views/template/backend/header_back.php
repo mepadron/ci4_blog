eca343db4286d64a4be50bed2cdec42478d1c466
Miguel Padrron <mikyeduards@gmail.com>
2021-11-05 09:41:25 -0400

    add tailwind css puro a dasboard

--- app/Views/template/backend/header_back.php
+++ app/Views/template/backend/header_back.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plantilla Backend</title>
+    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
 </head>
