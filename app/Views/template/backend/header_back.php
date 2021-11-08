1fc6d79b5cb71d87337631a2017841f1fb8b901b
Miguel Padrron <mikyeduards@gmail.com>
2021-11-08 11:02:08 -0400

    add css desde public

--- app/Views/template/backend/header_back.php
+++ app/Views/template/backend/header_back.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plantilla Backend</title>
     <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link href="<?= base_url();?>/css/tailwind.min.css" rel="stylesheet">
 
 </head>
 <body>
